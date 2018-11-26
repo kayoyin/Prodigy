4e0e8a931c07e6ac0810d930601f450019b8a6af
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2018-11-26 14:04:14 +0100

    predict
    code that uses a trained LSTM model to compose music

---
+++ predict.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Prodigy
+//
+//  Created by Kayo Yin on 26/11/2018.
+//  Copyright © 2018 Kayo Yin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
