e75873b8ad26334b9f8afd227b066e88f019299b
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2018-12-19 08:58:01 +0100

    Create install_mlpack.bash

---
+++ install_mlpack.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# install boost
+sudo apt-get install -y libboost-all-dev
+
+sudo apt-get install -y txt2man
+
+# install cython
+sudo apt-get install -y python-pip
+sudo pip install cython
+
+pushd .
+wget http://sourceforge.net/projects/arma/files/armadillo-9.200.4.tar.xz
+tar xJf armadillo-9.200.4.tar.xz
+cd armadillo-9.200.4
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+popd
+rm -rf armadillo-9.200.4
+rm armadillo-9.200.4.tar.xz
+
+pushd .
+wget https://github.com/mlpack/mlpack/archive/mlpack-3.0.4.tar.gz
+tar xzf mlpack-3.0.4.tar.gz
+cd mlpack-mlpack-3.0.4
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+popd
+rm mlpack-3.0.4.tar.gz
+rm -rf mlpack-mlpack-3.0.4
