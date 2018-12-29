a220aa4ae294e3f1819317b7441dfc7befa9cc50
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2018-12-29 22:50:07 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -183,7 +183,7 @@ void predictNotes(RNN<MeanSquaredError<>>& model,
     	for (unsigned int j = 0; j < sequence_length; j++)
 	{
 		music(i+j,0) = notes(0,j);
-		start(0,0,j) = notes(0,j); \\update start to continue generation
+		start(0,0,j) = notes(0,j); // update start to continue generation
 	}
 	
     }
