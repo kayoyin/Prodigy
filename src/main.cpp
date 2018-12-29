3c17c80cf7011bd8ade60973924b41d6babe09f7
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2018-12-29 21:35:59 +0100

    trying different labels format

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,10 @@ arma::mat getReal(const mat& tempDataset, const int& sequence_length)
 // Generate array with 1 in the indice of the note present at a time step
 arma::cube getTrainY(const mat& tempDataset, const int& size_notes, const int& sequence_length)
 {
-    cube proba = cube(size_notes, tempDataset.n_rows - sequence_length, sequence_length, fill::zeros);
+    cube proba = cube(1, tempDataset.n_rows - sequence_length, 1);
     for (unsigned int i = sequence_length; i < tempDataset.n_rows; i++)
     {
-	int note = tempDataset.at(i,0);
-	proba.tube(note,i-sequence_length).fill(1);
+	proba(0,i,0) = tempDataset(i,0);
     }
     return proba;
 }
