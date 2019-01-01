1e210e14c95460f6385994655a18f30083f93acf
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-01-02 00:12:40 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ arma::mat getNotes(const mat& proba)
     mat notes = mat(1, num_notes);
     for (unsigned int i = 0; i < num_notes; i++)
     {
-        notes(0,i) = index_max(proba.col(i));
+        // notes(0,i) = index_max(proba.col(i));
 	notes(0,i) = arma::as_scalar(arma::find(arma::max(proba.col(i)) == proba.col(i), 1) + 1);
     }
     return notes;
