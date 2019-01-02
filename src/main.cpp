b47353f1dc5b37047e73d029811cdb4262fa48e1
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-01-02 19:42:54 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ arma::cube getTrainY(const mat& tempDataset, const int& sequence_length)
     for (unsigned int i = sequence_length; i < tempDataset.n_rows; i++)
     {
 	int note = tempDataset.at(i,0);
-	trainY.tube(0,0,i-sequence_length) = note+1;
+	trainY(0,0,i-sequence_length) = note+1;
     }
     return trainY;
 }
