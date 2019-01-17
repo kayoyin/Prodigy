12fbe62f8da0ed3d464c848a3c91fcc4d3b0fca9
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-01-17 10:49:35 +0100

    Update and rename composesilent.cpp to composegui.cpp

--- src/composegui.cpp
+++ src/composegui.cpp
@@ -25,24 +25,43 @@ void predictNotes(RNN<>& model,
 {
     
     cube start = cube(1, 1, sequence_length); // we initialize generation with a sequence of random notes
+    mat music = mat(size_music,1, fill::zeros);	
+
+    /**
     for (unsigned int i = 0; i < sequence_length; i++)
     {
-	start(0,0,i) = rand() % size_notes + 1; // random integer between 1 and size_notes
-    }	
+    	int note = rand() % size_notes + 1; // random integer between 1 and size_notes
+	start(0,0,i) = note
+	music(i,0) = note
+    }
 	
-    mat music = mat(size_music,1, fill::zeros);	
+    cout << "Headstart: " << start << endl;
+    **/
+	    
+    mat startnotes;
+    // Load notes from user input
+    data::Load("../utils/startnotes.csv", startnotes, true);
+	
+    for (unsigned int i = 0; i < sequence_length; i++)
+    {
+	start(0,0,i) = startnotes(i,0);
+	music(i,0) = startnotes(i,0);
+    }
+    
+	
     cube compose;
-    for (unsigned int i = 0; i < size_music; i = i + sequence_length)	
+    for (unsigned int i = sequence_length; i < size_music; i = i + sequence_length)	
     {	    
- 
-    	model.Predict(start, compose);    
-    		    
+    	// Getting predictions after starting notes .
+    	model.Predict(start, compose);
+	    
+    	// Fetching the notes from probability vector generated.	    
     	for (unsigned int j = 0; j < sequence_length; j++)
 	{
 		int note = arma::as_scalar(arma::find(
           arma::max(compose.slice(j).col(0)) == compose.slice(j).col(0), 1)) + 1;
 		music(i+j,0) = note;
-		start(0,0,j) = note; 
+		start(0,0,j) = note; // update start to continue generation taking into account the sequence just predicted
 	}
 
 	
@@ -55,17 +74,19 @@ void predictNotes(RNN<>& model,
 int main () {
 
   mat tempDataset;
-  const int rho = 5;
-  data::Load("../utils/training.csv", tempDataset, true); 
-  const int size_notes = max(tempDataset.row(0)) + 1; 
-  const int sequence_length = rho; 
-  const int size_music = 300; 
+  const int rho = 5; // must be the same rho as the one used to train the model
+  data::Load("../utils/training.csv", tempDataset, true); // load training data to extract the note alphabet size
+  const int size_notes = max(tempDataset.row(0)) + 1; // number of different notes
+  const int sequence_length = rho; // length of sequence taken into consideration during training
+  const int size_music = 300; //must be a multiple of sequence_length
     
+  cout << "Loading trained model ..." << endl;
   RNN<> model(rho); 
   data::Load("model.xml", "model", model);	
   
-
+  cout << "Composing ..." << endl;
   predictNotes(model, sequence_length,size_notes, size_music);
+  cout << "Finished :)" << endl;
   
   return 0;
 }
