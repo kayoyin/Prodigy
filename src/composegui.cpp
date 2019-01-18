062a5ab5aea1bd229c8e31650b1a3f38f0b9c661
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-01-18 14:53:04 +0100

    Update composegui.cpp

--- src/composegui.cpp
+++ src/composegui.cpp
@@ -65,7 +65,7 @@ void predictNotes(RNN<>& model,
 	
     }
     cout << "Saving predicted notes to \"notes.csv\" ..." << endl;
-    data::Save("../utils/notes.csv", music); 
+    data::Save("../build-piano-Desktop_Qt_5_12_0_GCC_64bit-Debug/notes.csv", music); 
     cout << "Music saved to \"notes.csv\"" << endl;
 }
 
